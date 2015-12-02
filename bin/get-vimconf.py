60d2aebadb7ed79b8205921f560a0025612a74ee
Dimitri Merejkowsky <dmerejkowsky@aldebaran-robotics.com>
2015-12-02 13:20:42 +0100

    bin/get-vimconf: default to neovim install, use vim-plug

--- bin/get-vimconf.py
+++ bin/get-vimconf.py
@@ -10,17 +10,16 @@ import sys
 import posixpath
 
 import subprocess
-
-ON_WIN = sys.platform.startswith("win")
-
-VIM_BUNDLE = os.path.expanduser("~/.vim/bundle")
+import requests
 
 VIMRC_TEMPLATE = """\
 " Auto-generated code. Do not edit
 source {vimrc}
 source {vimrclocal}
 """
-
+NVIM_CONF_DIR = os.path.expanduser("~/.config/nvim")
+INIT_VIM = os.path.join(NVIM_CONF_DIR, "init.vim")
+VIMPLUG_URL = "https://raw.githubusercontent.com/junegunn/vim-plug/0.7.2/plug.vim"
 
 def mkdir_p(dest_dir):
     """ Contrary to os.makedirs, this
@@ -40,52 +39,46 @@ def get_backup_name(filename):
       return backup
 
 def backup_conf():
-    """ Backup vim configuration
+    """ Backup previous nvim configuration """
+    if os.path.exists(INIT_VIM):
+        backup = get_backup_name(INIT_VIM)
+        os.rename(INIT_VIM, backup)
+        print(INIT_VIM, "backuped to", backup)
 
-    """
-    if ON_WIN:
-        vimrc = os.path.expanduser(r"~\_vimrc")
-    else:
-        vimrc = os.path.expanduser("~/.vimrc")
-    if os.path.exists(vimrc):
-        backup = get_backup_name(vimrc)
-        os.rename(vimrc, backup)
-        print(vimrc, "backuped to", backup)
-
-def main():
-    """ Main entry point
-
-    """
-    clone_neobundle()
-    create_vimrc_files()
-
-def clone_neobundle():
-    mkdir_p(VIM_BUNDLE)
-    readme_md = os.path.join(VIM_BUNDLE, "neobundle.vim", "README.md")
-    if not os.path.exists(readme_md):
-        cmd = ["git", "clone", "https://github.com/Shougo/neobundle.vim"]
-        subprocess.check_call(cmd, cwd=VIM_BUNDLE)
+def fetch_plug_vim(dest):
+    data = requests.get(VIMPLUG_URL)
+    with open(dest, "w") as fp:
+        fp.write(data.text)
 
 def create_vimrc_files():
     backup_conf()
 
-    # Create ~/.vimrc.local
-    vimrclocal = os.path.expanduser("~/.vimrc.local")
+    vimrclocal = os.path.join(NVIM_CONF_DIR, "vimrc.local")
     if not os.path.exists(vimrclocal):
         with open(vimrclocal, "w") as fp:
             fp.write('" Put your local settings here\n"')
 
-    # Copy the vimrc file in this repo
     this_dir = os.path.dirname(__file__)
     vimrc_src = os.path.join(this_dir, "..", "vimrc")
     vimrc_src = os.path.abspath(vimrc_src)
     to_write = VIMRC_TEMPLATE.format(vimrc=vimrc_src,
                                      vimrclocal=vimrclocal)
 
-    vimrc_dest = os.path.expanduser("~/.vimrc")
+    vimrc_dest = os.path.expanduser(INIT_VIM)
     with open(vimrc_dest, "w") as fp:
         fp.write(to_write)
 
+def main():
+    """ Main entry point
+
+    """
+    nvim_conf_dir = os.path.dirname(INIT_VIM)
+    autoload = os.path.join(nvim_conf_dir, "autoload")
+    mkdir_p(autoload)
+    vim_plug = os.path.join(autoload, "plug.vim")
+    fetch_plug_vim(vim_plug)
+    backup_conf()
+    create_vimrc_files()
 
 if __name__ == "__main__":
     main()
