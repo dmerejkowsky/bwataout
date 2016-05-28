2b7365227df2748ffab8fb49571a4b8b25aa5768
Dimitri Merejkowsky <d.merej@gmail.com>
2016-05-28 17:52:06 +0200

    vim_wrapper: parse filespecs with --remote too

--- vim/vim_wrapper.py
+++ vim/vim_wrapper.py
@@ -14,12 +14,13 @@ except ImportError:
 SOCKET_PATH="/tmp/neovim"
 
 def remote_nvim(filespecs):
-    if not len(filespecs) == 1:
-        sys.exit("Expecting exactly one argument")
-    file = filespecs[0]
-    file = os.path.abspath(file)
     nvim = neovim.attach("socket", path=SOCKET_PATH)
-    nvim.command(":e %s" % file)
+    to_open = parse_filespecs_for_remote(filespecs)
+    nvim.command(":tabnew")
+    for fullpath, line, column in to_open:
+        nvim.command(":e %s" % fullpath)
+        nvim.feedkeys("%iG" % line)
+        nvim.feedkeys("%i|" % column)
 
 def find_nvim():
     """ Try to find nvim in $PATH.
@@ -35,7 +36,7 @@ def find_nvim():
 def main_nvim(nvim_path, filespecs, diff=False):
     env = os.environ.copy()
     env["NVIM_LISTEN_ADDRESS"] = SOCKET_PATH
-    parsed = parse_filespecs(filespecs)
+    parsed = parse_filespecs_for_cmdline(filespecs)
     cmd = [nvim_path]
     if diff:
         cmd.append("-d")
@@ -43,18 +44,54 @@ def main_nvim(nvim_path, filespecs, diff=False):
     rc = subprocess.call(cmd, env=env)
     sys.exit(rc)
 
-def parse_filespecs(filespecs):
+def parse_filespecs_for_cmdline(filespecs):
+    """ Return a list of command line arguments
+    suitable to open the file at the correct line
+    number.
+
+    Due to vim command line API limitations, this
+    only works for lists of one element...
+
+    >>> parse_filespecs_for_cmdline(["foo.cpp:42"])
+    ["foo.cpp", "+42"]
+
+    """
     if len(filespecs) == 1:
         filespec = filespecs[0]
         if ":" in filespec:
-            splitted = filespec.split(":")
-            line = splitted[1]
-            filename = splitted[0]
+            parts = filespec.split(":")
+            line = parts[1]
+            filename = parts[0]
             return ["+%s" % line, filename]
         else:
             return [filespec]
     else:
         return filespecs
+
+def parse_filespecs_for_remote(filespecs):
+    """ Return a list of tuples
+    (full_path, line_number, column_number)
+
+    >>> parse_filespecs_for_remote(["foo.cpp:42:3"])
+    [("/path/to/foo.cpp", 42, 3)]
+    >>> parse_filespecs_for_remote(["foo.cpp:42"])
+    [("/path/to/foo.cpp", 42, 1)]
+    >>> parse_filespecs_for_remote(["foo.cpp"])
+    [("/path/to/foo.cpp", 1)]
+
+    """
+    res = list()
+    for filespec in filespecs:
+        parts = filespec.split(":")
+        parts += ["1"] * (3 - len(parts))
+        parts[0] = os.path.abspath(parts[0])
+        for i in (1, 2):
+            try:
+                parts[i] = int(parts[i])
+            except ValueError:
+                sys.exit("Failed to parse %s" % filespec)
+        res.append(parts)
+    return res
 
 def main():
     # TODO: parse filespecs (foo.c:42) here too:
