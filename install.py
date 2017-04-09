3185f0c74aa4a192bb05b9f694a80f5625f152c3
Dimitri Merejkowsky <dmerej@lorient.local>
2017-04-09 13:33:51 +0200

    install.py: fix fzf URL

--- install.py
+++ install.py
@@ -135,7 +135,7 @@ def install_fzf():
         return
     print("Cloning fzf")
     cmd = ["git", "clone", "--quiet",
-           "git@github.com/dmerejkowsky:fzf.git",
+           "git@github.com:dmerejkowsky/fzf.git",
            dest]
     subprocess.check_call(cmd)
     install_path = os.path.join(dest, "install")
