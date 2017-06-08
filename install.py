dcc6c957ad3a58fec6237f4434169116b95fdac0
Dimitri Merejkowsky <dimitri.merejkowsky@tanker.io>
2017-06-08 17:09:04 +0200

    Rewrite install to use a yaml config file
    Aaaah. Much nicer

--- install.py
+++ install.py
@@ -1,237 +1,91 @@
-""" Install the dot files """
-
-from __future__ import print_function
-
 import argparse
-import os
 import subprocess
-import sys
+from urllib.request import urlretrieve
+
+import path
+import ruamel.yaml
+
+import ui
 
 
-if sys.version_info.major == 2:
-    from urllib import urlretrieve
-else:
-    from urllib.request import urlretrieve
+class Executor:
+    def __init__(self):
+        self.conf = ruamel.yaml.safe_load(path.Path("conf.yml").text())
+        self.this_dir = path.Path(".").abspath()
+        self.home = path.Path("~").expanduser()
+
+    def do_clone(self, url, dest):
+        dest = path.Path(dest).expanduser()
+        dest.parent.makedirs_p()
+        if dest.exists():
+            ui.info_2("Skipping", dest.relpath(self.home))
+            return
+        ui.info_2("Cloning", url, "->", dest.relpath(self.home))
+        subprocess.check_call(["git", "clone", url, dest])
 
 
-THIS_DIR = os.path.abspath(os.path.dirname(__file__))
+    def do_fetch(self, url, dest):
+        dest = path.Path(dest).expanduser()
+        dest.parent.makedirs_p()
+        if dest.exists():
+            ui.info_2("Skipping", dest.relpath(self.home))
+            return
+        ui.info_2("Fetching", url, "->", dest.relpath(self.home))
+        urlretrieve(url, dest)
 
-def mkdir_p(path):
-    if not os.path.exists(path):
-        os.makedirs(path)
+    def do_write(self, src, contents):
+        src = path.Path(src).expanduser()
+        if src.exists():
+            ui.info_2("Skipping", src.relpath(self.home))
+            return
+        ui.info_2("Creating", src)
+        src.parent.makedirs_p()
+        contents = contents.format(this_dir=self.this_dir, home=self.home)
+        src.write_text(contents)
 
+    def do_symlink(self, src, dest):
+        src = self.this_dir.joinpath(src)
+        dest = path.Path(dest).expanduser()
+        dest.parent.makedirs_p()
+        if dest.exists():
+            ui.info_2("Skipping", dest.relpath(self.home))
+            return
+        ui.info_2("Symlink", dest.relpath(self.home))
+        src.symlink(dest)
 
-def create_symlink_if_missing(src, dest):
-    if os.path.exists(src):
-        print("Skipping", src)
-        return
-    dirname = os.path.dirname(src)
-    mkdir_p(dirname)
-    print(src, "->", dest)
-    os.symlink(dest, src)
+    def do_exec(self, args):
+        ui.info_2("Running", "`%s`" % " ".join(args))
+        fixed_args = [x.format(home=self.home) for x in args]
+        subprocess.check_call(fixed_args)
 
+    def install(self, program):
+        ui.info(ui.green, program)
+        ui.info("-" * len(program))
+        todo = self.conf[program]
+        for action in todo:
+            name = list(action.keys())[0]
+            args = action[name]
+            func = getattr(self, "do_%s" % name)
+            func(*args)
+        ui.info()
 
-def write_file_if_missing(path, contents):
-    if os.path.exists(path):
-        print("Skipping", path)
-        return
-    print("Creating", path)
-    dirname = os.path.dirname(path)
-    mkdir_p(dirname)
-    with open(path, "w") as fp:
-        fp.write(contents)
-
-
-def vim_install(enable_vim=False):
-    nvim_conf_dir = os.path.expanduser("~/.config/nvim")
-    autoload = os.path.join(nvim_conf_dir, "autoload")
-    mkdir_p(autoload)
-
-    # Fetch vim plug
-    vim_plug = os.path.join(autoload, "plug.vim")
-    print("Retrieving", vim_plug)
-    urlretrieve("https://raw.githubusercontent.com/junegunn/vim-plug/0.7.2/plug.vim",
-                vim_plug)
-
-    # Create vimrc files
-    vimrc_local = os.path.join(nvim_conf_dir, "vimrc.local")
-    write_file_if_missing(vimrc_local, '" Put your local settings here\n')
-
-    vimrc_src = os.path.join(THIS_DIR, "vim", "vimrc")
-    vimrc_src = os.path.abspath(vimrc_src)
-    vimrc_template = """\
-" Auto-generated code. Do not edit\n"
-source {vimrc}\n"
-source {vimrc_local}\n"
-"""
-    to_write = vimrc_template.format(vimrc=vimrc_src,
-                                     vimrc_local=vimrc_local)
-    init_vim = os.path.join(nvim_conf_dir, "init.vim")
-    write_file_if_missing(init_vim, to_write)
-
-    # Create symlinks for vim usage
-    if enable_vim:
-
-        # ~/.vimrc -> ~/.config/nvim/init.vim
-        vimrc = os.path.expanduser("~/.vimrc")
-        create_symlink_if_missing(vimrc, init_vim)
-
-        # ~/.vim/autoload/plug.vim -> ~/.config/nvim/autoload/plug.vim
-        autoload = os.path.expanduser("~/.vim/autoload")
-        src = os.path.join(autoload, "plug.vim")
-        dest = vim_plug
-        create_symlink_if_missing(src, dest)
-
-    # Install the Python wrapper:
-    src = os.path.expanduser("~/.local/bin/vim_wrapper.py")
-    dest = os.path.join(THIS_DIR, "vim", "vim_wrapper.py")
-    create_symlink_if_missing(src, dest)
-
-    # Run vim once with :PlugInstall to perform first installation
-    if enable_vim:
-        vim_exec = "vim"
-    else:
-        vim_exec = "nvim"
-    cmd = [vim_exec, "-c", ":PlugInstall"]
-    subprocess.call(cmd)
+    def execute(self, programs=None):
+        if not programs:
+            programs = sorted(self.conf.keys())
+        for program in programs:
+            self.install(program)
 
 
-def zsh_install_pure_prompt():
-    """ Install zsh pure prompt from my own fork """
-    # All we have to do is copy to files from the repo
-    # to ~/.local/share/zsh/pure-prompt/ with the correct name
-    zsh_prompt_dir = os.path.expanduser("~/.local/share/zsh/pure-prompt/")
-    mkdir_p(zsh_prompt_dir)
-    base_url = "https://raw.githubusercontent.com/dmerejkowsky/zsh-pure-prompt"
-    base_url += "/master"
-    todo = [("pure.zsh", "prompt_pure_setup"),
-            ("async.zsh", "async")]
-    for (src, dest) in todo:
-        url = os.path.join(base_url, src)
-        full_dest = os.path.join(zsh_prompt_dir, dest)
-        print("Retrieving", full_dest)
-        urlretrieve(url, full_dest)
-
-
-def install_fzf():
-    dest = os.path.expanduser("~/.fzf")
-    if os.path.exists(dest):
-        print("Skipping", dest)
-        return
-    print("Cloning fzf")
-    cmd = ["git", "clone", "--quiet",
-           "git@github.com:dmerejkowsky/fzf.git",
-           dest]
-    subprocess.check_call(cmd)
-    install_path = os.path.join(dest, "install")
-    cmd = ["bash", install_path, "--bin"]
-    subprocess.check_call(cmd)
+def install(programs=None):
+    executor = Executor()
+    executor.execute(programs=programs)
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--enable-vim", action="store_true",
-                        help="Enable vim usage. Useful when neovim is not "
-                             "available")
-    parser.set_defaults(enable_vim=False)
+    parser.add_argument("programs", nargs="*")
     args = parser.parse_args()
-
-    mkdir_p(os.path.expanduser("~/.local/bin"))
-    mkdir_p(os.path.expanduser("~/.config"))
-
-    # conky
-    src = os.path.expanduser("~/.config/conky/conky.conf")
-    dest = os.path.join(THIS_DIR, "conky.conf")
-    if sys.platform.startswith("linux"):
-        create_symlink_if_missing(src, dest)
-
-    # git
-    gitconfig_dir = os.path.expanduser("~/.config/git")
-    gitconfig = os.path.join(gitconfig_dir, "config")
-    write_file_if_missing(gitconfig, """\
-# Autogenerated. Do not edit
-[core]
-excludesfile = {0}/gitexcludes
-
-[include]
-path = {0}/gitconfig
-path = {1}/config.local
-""" .format(THIS_DIR, gitconfig_dir))
-    gitconfig_local = gitconfig + ".local"
-    write_file_if_missing(gitconfig_local, """\
-# Configure your user here
-[user]
-name =
-email =
-
-
-# vim: set filetype=gitconfig:
-""")
-
-    # i3
-    src = os.path.expanduser("~/.config/i3/config")
-    dest = os.path.join(THIS_DIR, "i3")
-    create_symlink_if_missing(src, dest)
-
-    # fzf
-    install_fzf()
-
-    # mpv
-    src = os.path.expanduser("~/.config/mpv/input.conf")
-    dest = os.path.join(THIS_DIR, "mpv/input.conf")
-    create_symlink_if_missing(src, dest)
-    src = os.path.expanduser("~/.config/mpv/mpv.conf")
-    dest = os.path.join(THIS_DIR, "mpv/mpv.conf")
-    create_symlink_if_missing(src, dest)
-
-    # npm
-    # allow to use `npm install -g` _without_ being root
-    src = os.path.expanduser("~/.npmrc")
-    write_file_if_missing(src, "prefix=%s\n" % os.path.expanduser("~/.local"))
-
-    # openbox
-    src = os.path.expanduser("~/.config/openbox")
-    dest = os.path.join(THIS_DIR, "openbox")
-    create_symlink_if_missing(src, dest)
-
-    # screen
-    src = os.path.expanduser("~/.screenrc")
-    dest = os.path.join(THIS_DIR, "screenrc")
-    create_symlink_if_missing(src, dest)
-
-    # sway
-    src = os.path.expanduser("~/.config/sway/config")
-    dest = os.path.join(THIS_DIR, "sway")
-    create_symlink_if_missing(src, dest)
-
-    # tint2
-    src = os.path.expanduser("~/.config/tint2/tint2rc")
-    dest = os.path.join(THIS_DIR, "tint2rc")
-    create_symlink_if_missing(src, dest)
-
-    # vim / neovim
-    vim_install(enable_vim=args.enable_vim)
-
-    # xinitrc
-    src = os.path.expanduser("~/.xinitrc")
-    dest = os.path.join(THIS_DIR, "xinitrc")
-    create_symlink_if_missing(src, dest)
-    xinitrc_local = src + ".local"
-    write_file_if_missing(xinitrc_local, """\
-# Run your window manager from here
-""")
-
-    # zsh
-    zshrc = os.path.expanduser("~/.zshrc")
-    zshrc_local = zshrc + ".local"
-    write_file_if_missing(zshrc, """\
-# Auto-generated. Do not edit
-source {0}/zshrc
-if [ -f "${{HOME}}/.zshrc.local" ] ; then
-  source "${{HOME}}/.zshrc.local"
-fi
-""".format(THIS_DIR))
-    zsh_install_pure_prompt()
+    install(programs=args.programs)
 
 
 if __name__ == "__main__":
---
+++ install.sh
@@ -0,0 +1,4 @@
+curl -O https://bootstrap.pypa.io/get-pip.py
+python3 get-pip.py --user
+python3 -m pip install ruamel.yaml path.py --user
+python3 install.py
