e6c9a9b3bf78c7c2a579b4b3524efb86ade6476f
Dimitri Merejkowsky <d.merej@gmail.com>
2017-07-03 21:57:58 +0200

    bootstrap: install neovim too
    That way vim-plug is happier :)

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,3 @@
 curl -O https://bootstrap.pypa.io/get-pip.py
 python3 get-pip.py --user
-python3 -m pip install colorama unidecode ruamel.yaml path.py --user
+python3 -m pip install -r requirements.txt --user
