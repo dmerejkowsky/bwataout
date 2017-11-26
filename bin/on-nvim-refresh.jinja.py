bbe05af26fe27a6a771abdb491f1903fbc61df0b
Dimitri Merejkowsky <d.merej@gmail.com>
2017-11-26 14:31:39 +0100

    bin: add on-nvim-refresh script generator

---
+++ bin/on-nvim-refresh.jinja.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+{% if browser %}
+from selenium import webdriver
+{% endif %}
+import subprocess
+
+import neovim
+
+
+def main():
+    vim = neovim.attach("socket", path="/tmp/neovim")
+    vim.subscribe("refresh")
+    vim.command('nnoremap ,m :w<cr>:call rpcnotify(0, "refresh")<cr>')
+    {% if browser %}
+    url = "{{ url }}"
+    {% if browser == "chrome" %}
+    driver = webdriver.Chrome()
+    {% else %}
+    driver = webdriver.Firefox()
+    {% endif %}
+    driver.get(url)
+    {% endif %}
+    while True:
+        try:
+            vim.next_message()
+            {% if cmd %}
+            print("{{ cmd }}")
+            subprocess.run("{{ cmd }}", shell=True, check=True)
+            {% endif %}
+            {% if browser %}
+            driver.refresh()
+            {% endif %}
+        except Exception as e:
+            print(e)
+
+
+if __name__ == "__main__":
+    main()
