c6e8125e69988da00e0162cf65c40c2b05cb771a
Dimitri Merejkowsky <d.merej@gmail.com>
2019-02-12 21:17:04 +0100

    Re-use existing framework for history handling
    Because we can!

--- dm-tools/src/lib.rs
+++ dm-tools/src/lib.rs
@@ -6,6 +6,7 @@ use crate::cmd::SubCommand;
 
 pub enum CmdType {
     CwdHistory,
+    CommandsHistory,
     MruFiles,
 }
 
@@ -13,10 +14,12 @@ pub fn run_cmd(cmd_type: CmdType, cmd: SubCommand) {
     let name = match cmd_type {
         CmdType::CwdHistory => "cwd-history",
         CmdType::MruFiles => "mru-files",
+        CmdType::CommandsHistory => "commands-history",
     };
     let command = match cmd_type {
         CmdType::CwdHistory => "change-directory",
         CmdType::MruFiles => "edit -existing",
+        CmdType::CommandsHistory => "",
     };
     let mut app = crate::app::App::new(name);
     match cmd {
