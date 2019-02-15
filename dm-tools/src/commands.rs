89f5ee8bedc76cc552ae83765c86f009e260280b
Dimitri Merejkowsky <dimitri.merejkowsky@tanker.io>
2019-02-15 14:48:41 +0100

    Don't try and make command-history work from kakoune
    I need something better than that

--- dm-tools/src/commands.rs
+++ dm-tools/src/commands.rs
@@ -14,7 +14,7 @@ impl Commands{
 impl EntriesCollection for Commands {
     fn name(&self) -> String { "commands-history".to_string() }
 
-    fn kakoune_cmd(&self, entry: &str) -> String { format!("%sh{{ {} }}", entry) }
+    fn kakoune_cmd(&self, _entry: &str) -> String { "n/a".to_string() }
 
     fn add(&mut self, entry: &str) {
         self.entries = insert_last_and_dedup(&self.entries, entry);
