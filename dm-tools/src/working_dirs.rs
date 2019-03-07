7ad18504b089e128299eb9c236fe5e6e78eeabcb
Dimitri Merejkowsky <dimitri.merejkowsky@tanker.io>
2019-03-07 11:04:46 +0100

    Fix regression: use absolute path
    Note: clean is kinda broken now ...

--- dm-tools/src/working_dirs.rs
+++ dm-tools/src/working_dirs.rs
@@ -1,5 +1,6 @@
 use crate::list_helpers::*;
 use crate::storage::EntriesCollection;
+use std::path::Path;
 
 pub struct WorkingDirs {
     entries: Vec<String>,
@@ -12,13 +13,34 @@ impl WorkingDirs {
 }
 
 impl EntriesCollection for WorkingDirs {
-    fn name(&self) -> String { "cwd-history".to_string() }
+    fn name(&self) -> String {
+        "cwd-history".to_string()
+    }
 
-    fn list(&self) -> &Vec<String> { &self.entries }
+    fn list(&self) -> &Vec<String> {
+        &self.entries
+    }
 
-    fn kakoune_cmd(&self, entry: &str) -> String { format!("change-working-directory '{}'", entry) }
+    fn kakoune_cmd(&self, entry: &str) -> String {
+        format!("change-working-directory '{}'", entry)
+    }
 
     fn add(&mut self, entry: &str) {
+        let path = Path::new(entry);
+        if !path.exists() {
+            return;
+        }
+        let abs_path = if path.is_absolute() {
+            path.to_path_buf()
+        } else {
+            std::env::current_dir().unwrap().join(path)
+        };
+        let clean_path = abs_path
+            .canonicalize()
+            .unwrap_or_else(|_| panic!("Could not canonicalize {}", entry));
+        let entry = clean_path
+            .to_str()
+            .unwrap_or_else(|| panic!("Could not convert {} to string", clean_path.display()));
         self.entries = insert_last_and_dedup(&self.entries, entry);
     }
 
