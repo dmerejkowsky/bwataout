08b63d60abc04f85ad686b933e4ed0d771d2d0dd
Dimitri Merejkowsky <dimitri.merejkowsky@tanker.io>
2019-03-15 15:51:46 +0100

    More rust for the win

---
+++ dm-tools/src/bin/system-clipboard-paste.rs
@@ -0,0 +1,7 @@
+use clipboard::{ClipboardContext, ClipboardProvider};
+
+fn main() {
+    let mut ctx: ClipboardContext = ClipboardProvider::new().unwrap();
+    let contents = ctx.get_contents().unwrap();
+    print!("{}", contents);
+}
