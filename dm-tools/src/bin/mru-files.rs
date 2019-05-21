e3c3b66498a8d2a569234b6f94f7f981c9d1ad3d
Dimitri Merejkowsky <dimitri.merejkowsky@tanker.io>
2019-05-21 15:30:35 +0200

    Use correct command line parser for mru-files
    We were lucky they were compatible all this time ;)

--- dm-tools/src/bin/mru-files.rs
+++ dm-tools/src/bin/mru-files.rs
@@ -1,6 +1,6 @@
 use structopt::StructOpt;
 
 fn main() {
-    let cmd = dm_tools::cmd::CwdHistory::from_args();
+    let cmd = dm_tools::cmd::FilesHistory::from_args();
     dm_tools::run_cache_manager(dm_tools::StorageType::MruFiles, cmd.sub_cmd)
 }
