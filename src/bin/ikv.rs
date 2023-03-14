74a8e2db53e1d046e0f898f30a9c9d44068d818d
Dimitri Merejkowsky <dimitri@dmerej.info>
2023-03-14 20:05:08 +0100

    Cosmetic changes

--- src/bin/ikv.rs
+++ src/bin/ikv.rs
@@ -4,23 +4,18 @@ use chrono::prelude::*;
 fn main() -> Result<(), String> {
     let dir = get_ikv_dir_from_args()?;
     let map = parse_map(&dir)?;
-
     let now: DateTime<Local> = Local::now();
-
     let trips_md = read_trips_md(&dir, now)?;
-
     let trips = parse_trips(&trips_md);
-
-    let mut total = 0;
+    let mut total_distance = 0;
     for trip in trips {
-        let d = traveled_distance(&map, trip.places())?;
-        total += d;
-        println!("{d:2}km - {}", trip.description());
+        let distance = traveled_distance(&map, trip.places())?;
+        total_distance += distance;
+        println!("{distance:2}km - {}", trip.description());
     }
-
     let year = now.year();
     let month = now.month();
-    println!("---\nIKV for {year} {month:0>2} = {total}km");
+    println!("---\nIKV for {year} {month:0>2} = {total_distance}km");
 
     Ok(())
 }
